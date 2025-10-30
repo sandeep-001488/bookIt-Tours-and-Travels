13c7f3a4899ce629bf0000a0d89aa23b937bbe8a
sandeep <sandeepsanu1230@gmail.com>
2025-10-30 19:01:21 +0530

    added cors policy

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,14 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://book-it-flax.vercel.app", "http://localhost:3000"], 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
